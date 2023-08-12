6e80990b0f8fb8538c31e2a513fd5ef919140b88
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-08-12 14:22:04 +0530

    Completed the CRUD.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const connection = require('./config/db')
 const dotenv = require("dotenv")
 var bodyParser = require("body-parser");
+const e = require("cors");
 
 dotenv.config();
 
@@ -39,6 +40,7 @@ app.post("/create", (req, res) => {
     }
 });
 
+// Read 
 app.get("/data", (req, res) => {
     connection.query("Select * from crud", (error, rows) => {
         if (error) {
@@ -48,7 +50,58 @@ app.get("/data", (req, res) => {
             res.render("read.ejs", { rows });
         }
     })
+});
+
+// Delete
+app.get("/delete-data", (req, res) => {
+    const deleteQuery = "Delete from crud where id=?";
+
+    connection.query(deleteQuery, [req.query.id], (error, rows) => {
+        if (error) {
+            console.log(error)
+        }
+        else {
+            res.redirect("/data")
+        }
+    })
 })
+
+// Fetch data
+app.get("/update-data", (req, res) => {
+    connection.query("Select * from crud where id=?", req.query.id, (error, eachRow) => {
+        if (error) {
+            console.log(error)
+        }
+        else {
+            const result = JSON.parse(JSON.stringify(eachRow[0]));
+            console.log(result);
+            res.render("update.ejs", {result})
+        }
+    })
+})
+
+// Update
+app.post("/final-update", (req, res) => {
+    console.log(req.body);
+    const id = req.body.id;
+    const name = req.body.name;
+    const email = req.body.email;
+    const updateQuery = "Update crud Set name=?, email=? where id=?"
+    try {
+        connection.query(updateQuery, [name, email, id], (error, rows) => {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                res.redirect("/data");
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
 
 
 app.listen(PORT || 4000 || 5000, (error) => {
